04bd7fd7a7d5f58052f4f57e36abe886c2f48df3
y2kpartytime <C24732831@myTUDublin.ie>
2026-02-26 18:33:11 +0000

    Combat Updates
    - Added "Shoot" and "Magic/cast" animation states
    - Changed MeleeCombat to CombatScript
    - Added more Animations
    - Stuff
    
    Search "Ted's folder" for assets + lmk if anything wrong

--- RPG TUD/Assets/_Scripts/Player/CombatScript.cs
+++ RPG TUD/Assets/_Scripts/Player/CombatScript.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
 using StarterAssets;
 
-public class MeleeCombat : MonoBehaviour
+public class CombatScript : MonoBehaviour
 {
     [Header("Combat Settings")]
     public float attackCooldown = 0.8f;
+    public float shootCooldown = 1f;
+    public float castCooldown = 3f;
     public int attackDamage = 25;
+    public int shootDamage = 15;
+    
 
     [Header("References")]
     public Animator animator;
     public Collider weaponCollider;
-
     private float lastAttackTime;
+    private float lastShootTime;
+    private float lastCastTime;
+
     private ThirdPersonController controller;
 
     public ParticleSystem swingParticles;
+    public ParticleSystem hitParticles;
+    public ParticleSystem shootParticles;
 
     void Awake()
     {
@@ -26,9 +34,17 @@ public class MeleeCombat : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) // Left-click for shooting
+        {
+            TryShoot();
+        }
+        else if (Input.GetMouseButtonDown(1)) // Right-click for melee attack
         {
             TryAttack();
+        }
+        else if (Input.GetKey(KeyCode.F)) // F key for magic
+        {
+            TryCast();
         }
     }
 
@@ -40,6 +56,27 @@ public class MeleeCombat : MonoBehaviour
         lastAttackTime = Time.time;
 
         animator.SetTrigger("Attack");
+        swingParticles.Play();
+    }
+
+    void TryShoot()
+    {
+        if (Time.time < lastShootTime + shootCooldown)
+            return;
+
+        lastShootTime = Time.time;
+
+        animator.SetTrigger("Shoot");
+    }
+
+    void TryCast()
+    {
+        if (Time.time < lastCastTime + castCooldown)
+            return;
+
+        lastCastTime = Time.time;
+
+        animator.SetTrigger("Cast");
     }
 
     // Called by animation event
@@ -54,11 +91,6 @@ public class MeleeCombat : MonoBehaviour
     {
         if (weaponCollider != null)
             weaponCollider.enabled = false;
-
     }
 
-    public void MeleeSwingEffect()
-    {
-        swingParticles.Play();
-    }
 }
