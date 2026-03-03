5c3eda2f343de997214ba1a3a2ebb540b6b30214
y2kpartytime <C24732831@myTUDublin.ie>
2026-03-03 01:28:12 +0000

    Combat and Camera updates
    - Title
    - Changed camera to Isometric/Orthographic + Hard look at + removed dampening
    - Rolling added (Code and anims) and Root motion for player
    - Tight controls (needs work on auto-aim)

--- RPG TUD/Assets/_Scripts/Player/CombatScript.cs
+++ RPG TUD/Assets/_Scripts/Player/CombatScript.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using StarterAssets;
+using System;
+using Cinemachine;
 
 public class CombatScript : MonoBehaviour
 {
@@ -9,7 +11,15 @@ public class CombatScript : MonoBehaviour
     public float castCooldown = 3f;
     public int attackDamage = 25;
     public int shootDamage = 15;
+    public bool isShooting = false;
+    public bool isCasting = false;
+    public bool isMeleeing = false;
     
+    [Header("Lock-on")]
+    public float lockRange = 20f;
+    public Transform currentTarget;
+    public float rotateSpeed = 25f;
+    public bool isLocked = false;
 
     [Header("References")]
     public Animator animator;
@@ -19,14 +29,19 @@ public class CombatScript : MonoBehaviour
     private float lastCastTime;
 
     private ThirdPersonController controller;
+    public CinemachineVirtualCamera cinemachineCamera;
 
     public ParticleSystem swingParticles;
     public ParticleSystem hitParticles;
     public ParticleSystem shootParticles;
+    public GameObject player;
+
+    
 
     void Awake()
     {
         controller = GetComponent<ThirdPersonController>();
+        player = GameObject.FindGameObjectWithTag("Player");
 
         if (weaponCollider != null)
             weaponCollider.enabled = false;
@@ -34,63 +49,112 @@ public class CombatScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // Left-click for shooting
+        //FindNearestEnemy();
+
+        /*if (isLocked && currentTarget != null)
+        {
+            float distanceToTarget = Vector3.Distance(transform.position, currentTarget.position);
+            if (distanceToTarget > lockRange)
+            {
+                UnlockTarget();
+            }
+        }*/
+
+
+        if (Input.GetMouseButtonDown(0))
         {
             TryShoot();
         }
-        else if (Input.GetMouseButtonDown(1)) // Right-click for melee attack
+        else if (Input.GetMouseButtonDown(1))
         {
             TryAttack();
         }
-        else if (Input.GetKey(KeyCode.F)) // F key for magic
+        else if (Input.GetKey(KeyCode.F))
         {
             TryCast();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("Roll");
         }
     }
 
     void TryAttack()
     {
-        if (Time.time < lastAttackTime + attackCooldown)
+        if (Time.time < lastAttackTime + attackCooldown && !isShooting && !isCasting)
             return;
 
         lastAttackTime = Time.time;
-
+        isMeleeing = true;
         animator.SetTrigger("Attack");
+        
+    }
+
+    void meleeParticles()
+    {
         swingParticles.Play();
     }
 
     void TryShoot()
     {
-        if (Time.time < lastShootTime + shootCooldown)
+        if (Time.time < lastShootTime + shootCooldown && !isMeleeing && !isCasting  )
             return;
 
         lastShootTime = Time.time;
 
+        isShooting = true;
         animator.SetTrigger("Shoot");
+        
     }
 
     void TryCast()
     {
-        if (Time.time < lastCastTime + castCooldown)
+        if (Time.time < lastCastTime + castCooldown && !isShooting && !isMeleeing)
             return;
 
         lastCastTime = Time.time;
 
+        isCasting = true;
         animator.SetTrigger("Cast");
+        
     }
 
-    // Called by animation event
-    public void EnableWeapon()
+    void ToggleSoftLock()
     {
-        if (weaponCollider != null)
-            weaponCollider.enabled = true;
+        if (currentTarget == null)
+            return;
+
+        isLocked = !isLocked;
+
+        if (isLocked)
+            cinemachineCamera.LookAt = currentTarget;
+        else
+            cinemachineCamera.LookAt = player.transform; // Reset camera to player
     }
 
-    // Called by animation event
-    public void DisableWeapon()
+    void FindNearestEnemy()
     {
-        if (weaponCollider != null)
-            weaponCollider.enabled = false;
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        Transform nearest = null;
+        float shortestDistance = lockRange;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance <= shortestDistance)
+            {
+                shortestDistance = distance;
+                nearest = enemy.transform;
+            }
+        }
+
+        currentTarget = nearest; // null if none in range
     }
 
+    void UnlockTarget()
+    {
+        isLocked = false;
+        currentTarget = null;
+    }
+   
 }
